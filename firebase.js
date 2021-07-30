1e5391d652736b07364608e81b96415ee2ea878d
Faruq Bakare <bakarefaruq.a@gmail.com>
2021-07-30 01:46:38 +0100

    setting up next auth

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWRDDo5RGlOpb1FhJNtLaMAhRqdDuj-AU",
+  authDomain: "doc-clone-a200e.firebaseapp.com",
+  projectId: "doc-clone-a200e",
+  storageBucket: "doc-clone-a200e.appspot.com",
+  messagingSenderId: "44432208401",
+  appId: "1:44432208401:web:89440f51410dbd70ca37b0",
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp()
+  : firebase.app(firebaseConfig);
+
+const db = app.firestore();
+
+export { db };
